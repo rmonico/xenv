42e2479e14d3c4cc226b0657df1c68c9b486f280
Rafael Monico <rmonico@moningrado>
2021-07-30 23:35:22 -0300

    Using argparse to parse the arguments

--- xenv.py
+++ xenv.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
-import sys
+import argparse
+from argparse_builder import ArgumentParserBuilder
 import os
 import shutil
+import sys
+import logger_wrapper
 
 XENV_HOME = os.environ['XENV_HOME']
 XENV_ENVIRONMENTS = f'{XENV_HOME}/environments'
@@ -10,55 +13,10 @@ HERE_ENV = '__here_env__'
 
 
 class Main(object):
-    def __init__(self, args):
-        self.args = self._parse_args(args)
-
-    def _parse_args(self, args):
-        parsed = {}
-
-        while len(args) > 0:
-            if args[0] == '--source-files-dir':
-                parsed['output_files_dir'] = args[1]
-
-                self._shift(args, 2)
-
-            elif args[0] == 'ls':
-                parsed['command'] = 'ls'
-
-                args = {}
-            elif args[0] == 'load':
-                parsed['command'] = 'load'
-                parsed['environment'] = args[1] if len(args) > 1 else HERE_ENV
-
-                self._shift(args, 2)
-            elif args[0] == 'off':
-                parsed['command'] = 'off'
-
-                self._shift(args, 1)
-            elif args[0] == 'reload':
-                parsed['command'] = 'reload'
-
-                self._shift(args, 1)
-            else:
-                self._error('XENV: Command not found: "' + '" "'.join(args) + '"')
-
-        return parsed
-
-    @staticmethod
-    def _shift(args, count=1):
-        for i in range(count):
-            if len(args) == 0:
-                return
-
-            args.pop(0)
-
-    @staticmethod
-    def _error(message):
-        print(message)
-        sys.exit(1)
-
     def run(self):
-        if self.args['command'] == 'ls':
+        args = self._parse_args()
+
+        if args.command == 'ls':
             trailing_chars = len(XENV_ENVIRONMENTS) + 1
 
             from glob import glob
@@ -71,22 +29,50 @@ class Main(object):
             ]
             # TODO Show a HERE_ENV if it exists in current directory
 
-        if self.args['command'] == 'load':
-            return self._load(self.args['environment'], force=False)
+        if args.command == 'load':
+            return self._load(args.environment,
+                              args.source_files_dir,
+                              force=False)
 
-        elif self.args['command'] == 'off':
-            return self._unload()
+        elif args.command == 'off':
+            return self._unload(args.source_files_dir)
 
-        elif self.args['command'] == 'reload':
-            unload_status = self._unload()
+        elif args.command == 'reload':
+            unload_status = self._unload(args.source_files_dir)
 
             if unload_status == 0:
                 return self._load(os.environ['XENV_ACTIVE_ENVIRONMENT'],
-                            force=True)
+                                  args.source_files_dir,
+                                  force=True)
             else:
                 return unload_status
 
-    def _load(self, environment, force):
+    def _parse_args(self):
+        '''
+        reference: https://docs.python.org/3/library/argparse.html
+        '''
+        parser = argparse.ArgumentParser()
+
+        parser.add_argument('--source-files-dir',
+                            help='Source files directory (internal use only)')
+
+        with ArgumentParserBuilder(parser) as b:
+            b.add_command('ls')
+            with b.add_command('load') as b:
+                b.add_argument('environment', default=HERE_ENV)
+            b.add_command('off')  # alias: unload (TODO)
+            b.add_command('reload')
+
+        logger_wrapper.make_verbosity_argument(parser)
+
+        return parser.parse_args()
+
+    @staticmethod
+    def _error(message):
+        print(message)
+        sys.exit(1)
+
+    def _load(self, environment, source_files_dir, force):
         if not force and self._xenv_has_loaded_environment():
             self._error('Xenv environment already loaded')
 
@@ -96,15 +82,16 @@ class Main(object):
         source = os.path.join(self._environmentdir(environment), 'load')
 
         if not os.path.exists(source):
-            self._error(f'Environment "{environment}" has no load script, aborting')
+            self._error(
+                f'Environment "{environment}" has no load script, aborting')
 
-        self._append_to_export_file(source)
+        self._append_to_export_file(source_files_dir, source)
 
         print(f'Environment "{environment}" loaded')
 
         return 0
 
-    def _unload(self):
+    def _unload(self, source_files_dir):
         if not self._xenv_has_loaded_environment():
             self._error('No xenv environment loaded')
 
@@ -116,7 +103,7 @@ class Main(object):
             self._error(
                 f'Environment "{environment}" has no unload script, aborting')
 
-        self._append_to_export_file(source)
+        self._append_to_export_file(source_files_dir, source)
 
         print(f'Environment "{environment}" unloaded')
 
@@ -131,12 +118,7 @@ class Main(object):
         else:
             return os.path.join(XENV_ENVIRONMENTS, environment)
 
-    def _append_to_export_file(self, filename):
-        if not 'output_files_dir' in args:
-            error('--source-files-dir not informed')
-        else:
-            output_files_dir = args['output_files_dir']
-
+    def _append_to_export_file(self, output_files_dir, filename):
         dest = os.path.join(output_files_dir, 'xenv.environment')
 
         with open(dest, 'a') as environmentfile:
@@ -145,7 +127,6 @@ class Main(object):
                     environmentfile.write(line)
 
 
-main = Main(sys.argv[1:])
+main = Main()
 
 sys.exit(main.run())
-
