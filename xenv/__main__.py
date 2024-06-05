971732dfae54c6508a8805ed00a14030f2925ae9
Rafael Monico <rmonico@notevia>
2024-06-04 23:06:52 -0300

    fix: exporting XENV_HOME on launch

--- xenv/__main__.py
+++ xenv/__main__.py
@@ -5,11 +5,24 @@ from argparse_decorations import Command
 argparse_decorations.init()
 
 
+def _xenv_home():
+    import os
+
+    config_home = os.environ.get(
+            'XDG_CONFIG_HOME',
+            os.path.join(os.environ['HOME'], '.config'))
+
+    return os.path.join(config_home, 'xenv')
+
+
 @Command('launch-zsh', help='Print the launch script for ZSH')
 def launch():
     # TODO Autodetect shell
     with open('scripts/launch.zsh') as launch_file:
         print(launch_file.read(), end='')
+
+    print()
+    print(f'export XENV_HOME="{_xenv_home()}"')
 
 
 if __name__ == '__main__':
