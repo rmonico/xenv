c0ba8b97a08ae433b9f418c38d54cfb02d020f8a
Rafael Monico <rmonico@notevia>
2024-06-12 13:44:00 -0300

    feat(yq): show file name on error message

--- xenv/yq.py
+++ xenv/yq.py
@@ -3,11 +3,15 @@ from subprocess import run, PIPE
 
 class YqException(Exception):
 
-    def __init__(self, process):
+    def __init__(self, process, filename):
         self._process = process
+        self._filename = filename
 
     def __str__(self):
-        return self._process.stderr.decode()
+        if self._process.stderr:
+            return self._process.stderr.decode()
+        else:
+            return f'Erro loading yaml file \"{self._filename}\"'
 
 
 def yq(filter, input_file_name):
@@ -17,4 +21,4 @@ def yq(filter, input_file_name):
         import yaml
         return yaml.safe_load(process.stdout.decode())
     else:
-        raise YqException(process)
+        raise YqException(process, input_file_name)
