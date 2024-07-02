df865ce9686ab6df0654c80df11c8270749e8fac
Rafael Monico <rmonico@notevia>
2024-07-02 16:30:01 -0300

    feat(autocomplete): script working

--- autocomplete.zsh
+++ autocomplete.zsh
@@ -1,10 +1,27 @@
-_xenv() {
-    local -a subcommands=()
-    _arguments '1: :(launch-zsh load unload list create config)'
+#compdef xenv
 
-    case "$words[2]" in
-        load)
-            _arguments "2: :($(xenv list --raw))"
+_xenv() {
+    case "${words[@]:0:-1}" in
+        "xenv")
+            local -a commands=(
+                'launch-zsh:Print the launch script for ZSH'
+                'load:Load a environment'
+                'unload:Unload the environment'
+                'list:List environments'
+                'create:Create a environment'
+                'config:Configuration management'
+            )
+
+            _describe 'command' commands
+            ;;
+
+        "xenv load")
+            local environments="$(xenv list --raw)"
+
+            _arguments "2:environments:($environments)"
+
+            # TODO Fazer do jeito abaixo, tem que adaptar o comando list pra devolver a descrição dos ambientes
+            # _describe 'environments' ($(xenv list --raw))
             ;;
     esac
 }
